13f4202887fa3e9a9eb390bb345d715167b91a97
Richard Lucas <lucas1richard@gmail.com>
2016-11-02 17:30:15 -0400

    Added forms

--- meadowlark.js
+++ meadowlark.js
@@ -5,7 +5,16 @@ var fortune = require('./lib/fortune.js');
 
 app.set('port', process.env.PORT || 3000);
 
-var handlebars = require('express-handlebars').create({ defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({ 
+  defaultLayout:'main',
+  helpers: {
+    section: function(name, options) {
+      if(!this._sections) this._sections = {};
+      this._sections[name] = options.fn(this);
+      return null;
+    }
+  }
+});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -15,8 +24,65 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(function(req, res, next) {
+  if(!res.locals.partials) res.locals.partials = {};
+  res.locals.partials.weatherContext = getWeatherData();
+  next();
+});
+
+app.use(require('body-parser').urlencoded({ extended:true }));
+
+// mocked weather data
+function getWeatherData(){
+  return {
+    locations: [
+      {
+        name: 'Portland',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)',
+      },
+      {
+        name: 'Bend',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Manzanita',
+        forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };
+}
+
 app.get('/', function(req, res) {
   res.render('home');
+});
+
+app.get('/thank-you', function(req, res) {
+  res.render('thank-you');
+});
+
+app.get('/newsletter', function(req, res) {
+  res.render('newsletter', {csrf:'CSRF token goes here'});
+});
+
+app.post('/process', function(req, res) {
+  if(req.xhr || req.accepts('json,html')==='json') {
+    console.log('Form (from querystring):' + req.query.form);
+    console.log('CSRF token (from hidden form field):' + req.body._csrf);
+    console.log('Name (from visible form field):' + req.body.name);
+    console.log('Email (from visible form field):' + req.body.email);
+    res.send({success:true});
+  } else {
+    res.redirect(303, '/thank-you');
+  }
 });
 
 app.get('/about', function(req, res) {
@@ -32,6 +98,20 @@ app.get('/tours/hood-river', function(req, res) {
 
 app.get('/tours/request-group-rate', function(req, res) {
   res.render('tours/request-group-rate');
+});
+app.get('/jquery-test', function(req, res){
+  res.render('jquery-test');
+});
+app.get('/nursery-rhyme', function(req, res){
+  res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme', function(req, res){
+  res.json({
+    animal: 'squirrel',
+    bodyPart: 'tail',
+    adjective: 'bushy',
+    noun: 'heck',
+  });
 });
 
 app.use(function(req, res, next){
